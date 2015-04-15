f898c618ee42e5fa6d6f1e9809ce7eea9b0e3824
Nicolás López <nicolaslopezj@me.com>
2015-04-15 18:22:16 -0300

    added listen

--- options.js
+++ options.js
@@ -4,6 +4,7 @@
 Options = {
   _values: {},
   _deps: {},
+  _listeners: {},
 }
 
 /**
@@ -14,6 +15,7 @@ Options.init = function(key, initialValue) {
   check(key, String);
   this._deps[key] = new Tracker.Dependency;
   this._values[key] = initialValue;
+  this._listeners[key] = [];
 }
 
 /**
@@ -23,6 +25,9 @@ Options.set = function(key, value) {
   if (!_.has(this._deps, key)) throw 'Option "' + key + '" is not initalized';
   this._values[key] = value;
   this._deps[key].changed();
+  _.each(this._listeners[key], function(func){
+    func(value)
+  });
 }
 
 /**
@@ -33,6 +38,21 @@ Options.arrayPush = function(key, value) {
   if (!_.isArray(this._values[key])) throw 'Option "' + key + '" is not an array';
   this._values[key].push(value);
   this._deps[key].changed();
+  _.each(this._listeners[key], function(func){
+    func(value)
+  });
+}
+
+/**
+ * Adds listeners to a option. The difference between this and tracker is that tracker
+ * waits till the app is idle to run the callback, this is instantly. I'm not saying
+ * it's better, but sometimes tracker is not compatible.
+ */
+Options.listen = function(key, value) {
+  check(key, String);
+  check(value, Function);
+  if (!_.has(this._deps, key)) throw 'Option "' + key + '" is not initalized';
+  this._listeners[key].push(value);
 }
 
 /**
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'nicolaslopezj:options',
   summary: 'Set app options reactively',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/nicolaslopezj/options'
 });
 
